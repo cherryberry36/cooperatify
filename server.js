a930de3f9158964fa7ef7247127c455648d0bb16
cherryberry36 <olufunmilayowilliams@gmail.com>
2021-12-29 14:34:46 +0100

    next coomit

--- server.js
+++ server.js
@@ -18,4 +18,4 @@ if (process.env.NODE_ENV === 'development'){
 const port= process.env.PORT || 4000
 
 
-app.listen(port,()=> console.log(`server is running on ${port}`))
+app.listen(port,()=> console.log(`server is running on ${port}`));
